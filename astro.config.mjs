4f0480896cc023411867d1661a4acd37e3cc69a2
degsz <125046749+degszz@users.noreply.github.com>
2026-06-04 18:05:39 -0300

    chore: update site URL to bionodum.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false })],
   output: 'static',
-  site: 'https://bionodum.vercel.app',
+  site: 'https://bionodum.com',
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
